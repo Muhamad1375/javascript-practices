4d9f3d2c353cc0ffae7dc431e0881fe071ab9451
mohammadali fallahzade <Mohammadali.fallahzade@gmail.com>
2021-05-03 09:31:26 +0430

    all files created

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var myname = 'Mohammadali';
+
+console.log(myname);
+
+var myname = 'Mohammadali fallahzade';
+
+console.log(myname);
+
+
+
+
+let myname1 = 'George'
+    myname2 = 'Joe';
+
+console.log(myname1);
+console.log(myname2);
+
+
+const myage = 25;
+
+console.log(myage);
