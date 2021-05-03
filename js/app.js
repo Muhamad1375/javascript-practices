9b9985fdf1e3e0a95ea81af8a59bad108529fe04
mohammadali fallahzade <Mohammadali.fallahzade@gmail.com>
2021-05-03 10:15:06 +0430

    varriables

--- js/app.js
+++ js/app.js
@@ -1,21 +1,51 @@
-var myname = 'Mohammadali';
-
-console.log(myname);
-
-var myname = 'Mohammadali fallahzade';
-
-console.log(myname);
-
-
-
-
-let myname1 = 'George'
-    myname2 = 'Joe';
-
-console.log(myname1);
-console.log(myname2);
-
-
-const myage = 25;
-
-console.log(myage);
+//varriable declaratrion 
+// var myname = 'Mohammadali';
+// console.log(myname);
+// var myname = 'Mohammadali fallahzade';
+// console.log(myname);
+// let myname1 = 'George'
+//     myname2 = 'Joe';
+// console.log(myname1);
+// console.log(myname2);
+// const myage = 25;
+// console.log(myage);
+//strings
+let name2 = 'Mohammadali' ;
+console.log(name2);
+console.log(typeof name2)
+//digit
+let age = 25;
+    age = -65;
+    age = '47';
+    age= 25.25;
+console.log(age);
+console.log(typeof age);
+//undefined
+let host = undefined;
+console.log(typeof undefined);
+//symbol
+let symbol = Symbol ('this is a symbol');
+console.log(typeof symbol);
+//null
+let jk = null;
+console.log(typeof jk);
+//array
+let myarray = ['HTML', 'JS', 'PHP'];
+console.log(myarray);
+console.log(typeof myarray);
+//object
+let myinfo = {
+    name3: 'Mohammadali',
+    age3: 25,
+    country: 'Iran',
+}
+console.log(myinfo);
+console.log(typeof myinfo);
+//date
+let date = new Date();
+console.log(date);
+console.log(typeof date);
+//bool
+let ok = true;
+console.log(ok);
+console.log(typeof ok);
